7b5a3551689c3829e7e685c186035a3f35257595
Emir Ozturk <emirozturk79@gmail.com>
2022-02-16 22:03:19 +0300

    remade lol
    its a lot better now

---
+++ js/copy.js
@@ -0,0 +1,12 @@
+function copyDiscord() {
+   navigator.clipboard.writeText("rime#2371") ;
+   var x = document.getElementById("toast");
+  
+   // Add the "show" class to DIV
+   x.className = "show";
+ 
+   // After 3 seconds, remove the show class from DIV
+   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+   
+  }
+  
