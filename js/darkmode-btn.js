19e70b785b5aedde1b65fd1c77da2b654548434b
Emir Ozturk <emirozturk79@gmail.com>
2022-05-26 18:53:57 +0300

    a

--- js/darkmode-btn.js
+++
@@ -1,7 +0,0 @@
-var sun = document.getElementById('sun');
-var moon = document.getElementById('moon');
-document.querySelector("#dark").onclick = function(e){
-        sun.classList.toggle("deactive");
-        moon.classList.toggle("deactive");
-darkmode.toggleDarkMode();
-}
