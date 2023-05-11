ee3fe9c25a7d8abcd86819d3bb693fb72553b40d
emir ozturk <emirozturk79@gmail.com>
2023-05-11 15:12:14 +0300

    updated cv

--- js/index.js
+++ js/index.js
@@ -12,16 +12,16 @@ function add_navbar_item(name, link, icon, color) {
   let navbar_inside =
     `<div class="nav-item" style="background-color: `+color+`;">
     <div class="nav-item-bg" ></div>
-    <a href="`+link+`"><i class="`+icon+`"></i>`+name+`</a>
+    <a href="`+link+`" target="_blank"><i class="`+icon+`"></i>`+name+`</a>
     </div>`;
 
   navbar.insertAdjacentHTML("beforeend", navbar_inside);
 }
-function add_card(card, card_text, card_link, card_desc,card_class) {
+function add_card(card, card_text, card_link, card_desc,card_class,target) {
   let Card = document.getElementById(card);
   let card_inside =`
   <div class="body-card-text">
-   <a href="`+card_link+`" class=" contrast">
+   <a href="`+card_link+`" class=" contrast" target="`+target+`">
       <h4 class="card-item-name">`+card_text+`</h4>
       <p class="card-item-desc `+card_class+`" id="hovered">`+card_desc+`</p>
       
@@ -92,41 +92,41 @@ add_card(
   "about-card",
   "Emir Ozturk",
   "#",
-  "IEU Computer programming student in turkey."
+  "IEU Computer programming student in turkey.","","_blank"
 );
 
-add_card("about-card","for cv","/cv_13122022.pdf","click here","cv-link");
+add_card("about-card","for cv","/cv.pdf","click here","cv-link","_blank");
 
 // projects
 add_card(
   "projects-card",
   "rimexe0.github.io",
   "https://github.com/rimexe0/rimexe0.github.io",
-  "this website"
+  "this website","","_blank"
 );
 add_card(
   "projects-card",
   "gameAge",
   "https://github.com/rimexe0/gameAge",
-  "steam inspired website for midterm made by mostly me"
+  "steam inspired website for midterm made by mostly me","","_blank"
 );
 add_card(
   "projects-card",
   "java media player",
   "https://github.com/rimexe0/java-media-player",
-  "This contains my frustration with javaFx and bearly working java code."
+  "This contains my frustration with javaFx and bearly working java code.","","_blank"
 );
 add_card(
   "projects-card",
   "gameshop",
   "https://github.com/rimexe0/gameshop",
-  "steam inspired game store app for my midterm"
+  "steam inspired game store app for my midterm","","_blank"
 );
 add_card(
   "projects-card",
   "oasis scraper",
   "https://github.com/rimexe0/oasis-scraper",
-  "Simple program that gets oasis weekly schedule and sends it to google calendar"
+  "Simple program that gets oasis weekly schedule and sends it to google calendar","","_blank"
 );
 
 // blog
@@ -134,6 +134,6 @@ add_card(
   "blog-card",
   "work in progress",
   "/mdread.html",
-  "this part is going to be the blog part where i post about stuff i make. But its not ready yet."
+  "this part is going to be the blog part where i post about stuff i make. But its not ready yet.","","_self"
 );
 
